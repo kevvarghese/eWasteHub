97e6a7732816e6480acc99cc58832f14b01b73f2
kevvarghese <164647596+kevvarghese@users.noreply.github.com>
2025-02-12 18:07:30 +0530

    Add files via upload
    updates with php code

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$host="localhost";
+$user="root";
+$pass="";
+$db="ewh1";
+$conn=new mysqli($host,$user,$pass,$db);
+if($conn->connect_error){
+    echo "Failed to connect DB".$conn->connect_error;
+}
+//echo"connection successful";
+
+?>
---
+++ dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>About Us</title>
+        <link rel="stylesheet" href="./basic_css.css">
+        <link rel="stylesheet" href="./aboutus.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="logo">
+                <a href="./index.html">
+                    <img src="./assets/images/logo2.png" alt="Logo">
+                </a>
+            </div>
+            <div class="shortcuts" style="width: auto;"l>
+                <a href="./index.html" id="index">
+                    <i class="glyphicon glyphicon-home large-icon"></i>
+                </a>
+            </div>
+            <div style="width: auto;">
+                <a href="./services.html" id="services">Services</a>
+            </div>
+            <div style="width: auto;">
+                <a href="./FAQ.html" id="FAQ">FAQ</a>
+            </div>
+            <div style="width: auto;">
+                <a href="./aboutus.html" id="about">About Us</a>
+            </div>
+            <div style="width: auto;">
+                <a href="./contact.html" id="contact">Contact</a>    
+            </div>
+            <div style="width: auto;">
+                <a href="./logout.php" id="login">Logout</a>
+            </div>            
+        </div>
+        <br>
+
+    <h1>Welcome to Your Dashboard</h1>
+
+    <script src="auth.js"></script> 
+
+</body>
+</html>
